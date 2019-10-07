d9150839df95136b0f16ba29e2faa167fe4affbc
Kennedy Kori <kennedykori47@gmail.com>
2019-10-08 02:28:00 +0300

    Add tests to ensure that null sources checking works

--- src/test/java/com/kori_47/events/SimpleEventTest.java
+++ src/test/java/com/kori_47/events/SimpleEventTest.java
@@ -17,4 +17,14 @@ public class SimpleEventTest {
 		// Check that the event source is the correct source
 		assertEquals(source, event.source());
 	}
+
+	@Test
+	public void testThrowsNullPointerException() {
+		// Create a null source
+		Object nullSource = null;
+		
+		// Check that a NullPointerException is thrown when a null source is passed
+		// to the SimpleEvent constructor
+		assertThrows(NullPointerException.class, () -> new SimpleEvent(nullSource));
+	}
 }
