79cb4613a056e0eea7badda49accd60910690e93
Kennedy Kori <kennedykori47@gmail.com>
2019-10-10 03:09:37 +0300

    Refactor ThreadedListenerTest to implement from ListenerTest

--- src/test/java/com/kori_47/events/ThreadedListenerTest.java
+++ src/test/java/com/kori_47/events/ThreadedListenerTest.java
@@ -6,39 +6,27 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;;
 
-public class ThreadedListenerTest {
+public class ThreadedListenerTest implements ListenerTest<ThreadedListener> {
 	
-	@Test
-	public void testAddHandler() {
-		// Create a listener
-		ThreadedListener listener = new ThreadedListener();
-		
-		// Create generic handlers
-		Handler<SimpleEvent> handler1 = e -> e.source();
-		Handler<SimpleEvent> handler2 = e -> e.toString();
-		
-		// Assert that the listener has no SimpleEvent handlers before we add them
-		assertFalse(listener.getHandlers(SimpleEvent.class).isPresent());
-		
-		// Add the first SimpleEvent handler
-		listener.addHandler(SimpleEvent.class, handler1);
-		
-		// Assert that the listener has 1 SimpleEvent handler
-		assertEquals(1, listener.getHandlers(SimpleEvent.class).get().size());
-		
-		// Add the second SimpleEvent handler
-		listener.addHandler(SimpleEvent.class, handler2);
-		
-		// Assert that the listener has 2 SimpleEvent handlers
-		assertEquals(2, listener.getHandlers(SimpleEvent.class).get().size());
-		
+	@Override
+	public void cleanUp(ThreadedListener listener) {
 		listener.dispose();
 	}
-
+	
+	@Override
+	public Stream<ThreadedListener> listenerProvider() {
+		ExecutorService service = Executors.newFixedThreadPool(3);
+		return Stream.of(
+				new ThreadedListener(),										// An instance of thread listener using the default constructor
+				new ThreadedListener(0),									// A thread listener with unlimited threads
+				new ThreadedListener(1, Executors.defaultThreadFactory()),	// A thread listener with a single thread and a custome thread factory
+				new ThreadedListener(service, true)							// A thread with a custom executor service
+			);
+	}
+	
 	@ParameterizedTest
 	@MethodSource("listenerProvider")
 	public void testDispose(ThreadedListener listener) {
@@ -53,15 +41,5 @@ public class ThreadedListenerTest {
 		
 		// Assert that the listener is now disposed
 		assertTrue(listener.isDisposed());
-	}
-	
-	public static Stream<ThreadedListener> listenerProvider() {
-		ExecutorService service = Executors.newFixedThreadPool(3);
-		return Stream.of(
-				new ThreadedListener(),										// An instance of thread listener using the default constructor
-				new ThreadedListener(0),									// A thread listener with unlimited threads
-				new ThreadedListener(1, Executors.defaultThreadFactory()),	// A thread listener with a single thread and a custome thread factory
-				new ThreadedListener(service, true)							// A thread with a custom executor service
-			);
 	}
 }
